661ac76c788ce71463c05c0f4296d6b23f4afdb1
Feng Feng <ffeng@bu.edu>
2023-03-12 10:49:03 -0400

    adding new changes: read clonotype data and update the renv.lock file too

--- R/IgSeq_data.R
+++ R/IgSeq_data.R
@@ -67,3 +67,74 @@ README<-paste0(README,"The data are for all six subjects. colData is \n")
 README<-paste0(README, "the metadata about sequences.\n")
 
 save(file=here(data.dir,"BCR_vdj.RData"), igData, colData, README)
+
+
+#now I start to read enclone clonotype data.
+# the data were in the Data folder >> sub folder "enclone"
+data.dir<-"Data/enclone"
+filename<- rep("encloneOut.txt",6)
+
+#subjects, 6 
+subs<-c("S1","S2",
+		"S3","S4",
+		"S5","S6"	)
+#subs<-rep(subs, rep(3,6)); # each subject has 3 b cell populations
+
+#cells<-c("CD38Low","CD38Mid","CD38High")
+#cells<-rep(cells,6)
+
+disease<-c("healthy","cvid","cvid","healthy","cvid","healthy")
+#disease<-rep(disease, rep(3,6))
+
+paths<-c("PJ02",
+	"PJ03","PJ04","PJ0726","PJ0729","PJ1115")
+
+#loading subject 1. high/low/mid
+
+rowData<-data.frame()
+cloneData<-data.frame()
+last.three<-c()
+#read all the files
+for(i in 1:length(paths))
+{
+	#reading data set
+	cat("reading subs ", subs[i], 
+			"; dir :", paths[i],"\n")
+	#x<-read_csv(here(data.dir, paths[i],filename[i]))
+	x<-read.table(file=here(data.dir, paths[i],filename[i]),
+			sep=",", header=T, quote="\"'"
+		)
+	len<-length(names(x))
+	last.three<-c(last.three,names(x)[c(len-2,len-1,len)])
+	names(x)[c(len-2,len-1,len)]<-paste0("barcode",c(1:3))
+	c<-data.frame(cid=paste0(x$group_id,"-",x$clonotype_id,"-",
+			x$exact_subclonotype_id), subs=subs[i],
+			cells=cells[i],disease=disease[i])
+	c$cid<-paste0(c$cid,"-",subs[i])
+	rowData<-rbind(rowData,c)
+	x$cid<-c$cid #<-paste0(x$barcode,"-",subs[i])
+	x$subs=subs[i]
+	#x$cells=cells[i]
+	x$disease=disease[i]
+	cloneData<-rbind(cloneData,x)
+
+}
+#now, let's save them.
+README<-"the clonotyping data in a big table. this \n"
+README<-paste0(README, "is the output from enclone. \n")
+README<-paste0(README, "I so far grouped/pooled all 3\n")
+README<-paste0(README, "samples within each subject together\n")
+README<-paste0(README, "to increase the chanace of bigger\n")
+README<-paste0(README, "clones. I also will use the\n")
+README<-paste0(README, "\"group\", which is bigger, to\n")
+README<-paste0(README, "to get more clones. this is supposed\n")
+README<-paste0(README, "to be different from clonotype,\n")
+README<-paste0(README, "but I don't know what it is about\n")
+README<-paste0(README, "it is said to be a functional similar\n")
+README<-paste0(README, "rather than evolutionary\n")
+README<-paste0(README, "check enclone for more (there is \n")
+README<-paste0(README, "very little detail about it\n")
+README<-paste0(README, "About the data last.three, it is the order\n")
+README<-paste0(README, "of the last 3 fields of each enclone output, about the barcodes in 3 samples in our case.\n")
+README<-paste0(README, "IN it, it follows the order of s1-s6 in trios\n")
+save(file=here(data.dir,"clonotypeEnclone.RData"), cloneData, rowData, last.three, README)
